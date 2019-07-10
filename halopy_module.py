7db22dd20054d12009e0eb8a9054f6aa60f797cc
Divya Rana <divya.rana@niser.ac.in>
2019-07-10 18:17:41 +0530

    last updated

--- halopy_module.py
+++ halopy_module.py
@@ -53,7 +53,7 @@ class halo(constants):
     def init_sigma_spl(self):
 
         print "SPLINE READY FOR NFW SIGMA"
-        Rarr = np.logspace(-2,np.log10(5*self.r_200),30)
+        Rarr = np.logspace(-2,np.log10(5*self.r_200),70)
         Sigmaarr = Rarr*0.0
 
         for ii, R in enumerate(Rarr):
@@ -83,6 +83,8 @@ class halo(constants):
 
         if r > self.sigma_cir_dict["Rmax"]:
             value = 0.0
+        elif r < self.sigma_cir_dict["Rmin"]:
+            value = self.sigma_cir_dict["Sigmamin"]
 
         else:
             value = self.sig_cir_spl(r)
@@ -92,7 +94,7 @@ class halo(constants):
     def init_sigma_cir_spl(self,r0):
         """spline for the satellite at a distance r0 from the center for parents contribution averaged over a circle"""
         print "SPLINE READY FOR AVERAGING OVER CIRCLE"
-        rdbin = np.linspace(0,5*self.r_200,70)
+        rdbin = np.logspace(-2,np.log(5*self.r_200),50)
         des_cir = 0.0*rdbin
         for i  in range(0,len(rdbin)):
             des_cir[i] = quad(lambda j: self.sigma(np.sqrt(r0**2 + rdbin[i]**2 + 2*r0*rdbin[i]*np.cos(j))), 0., 2*np.pi)[0]/(2*np.pi)
@@ -100,6 +102,8 @@ class halo(constants):
         self.sig_cir_spl = interp1d(rdbin,des_cir,kind = "cubic")
         self.sigma_cir_dict = {}
         self.sigma_cir_dict["Rmax"] = rdbin[-1]
+        self.sigma_cir_dict["Rmin"] = rdbin[0]
+        self.sigma_cir_dict["Sigmamin"] = des_cir[0]
         self.init_sigma_cir = True
         return
 
@@ -112,11 +116,11 @@ class halo(constants):
 if __name__ == "__main__":
     """initiating the class"""
     h_p = halo(1e14,10.0)
-    h_d = halo(1e14,10.0)
+    h_d = halo(1e12,10.0)
     print 'hello'
-    rd_dist = 0.0#h_p.r_200/2
+    rd_dist = h_p.r_200/4
 
-    rdbin = np.logspace(-2,np.log10(h_d.r_200),20)
+    rdbin = np.logspace(-2,np.log10(2*h_p.r_200),50)
 
     delta_part = 0.0 * rdbin
     delta_daug = 0.0 * rdbin
@@ -124,22 +128,23 @@ if __name__ == "__main__":
    # print h_p.delta_sigma_dau(rdbin[0],rd_dist)
 
     for i in range(0,len(rdbin)):
-        delta_part[i] = h_p.sigma_cir(rdbin[i],rd_dist)
-        delta_daug[i] = h_d.sigma(rdbin[i])
+        delta_part[i] = h_p.delta_sigma_dau(rdbin[i],rd_dist)
+        delta_daug[i] = h_d.delta_sigma(rdbin[i])
 
         #print rdbin[i]
 
     #print h_d.delta_sigma(rdbin[0])
     #print h_p.delta_sigma_dau(rdbin[0],rd_dist)
 
-    plt.plot(rdbin,delta_daug/1e12 ,'r',label = 'daughter')
-    plt.plot(rdbin,delta_part/1e12,'b', label = 'parent')
-    #plt.plot(rdbin,(delta_sig_dau_nfw + dau_delta_sigma)/1e12,'g', label  = 'addition of both')
-    #plt.axvline(r0)
+    plt.plot(rdbin,delta_daug/1e12 ,'or',label = 'daughter')
+    plt.plot(rdbin,delta_part/1e12,'ob', label = 'parent')
+    plt.plot(rdbin,(delta_daug + delta_part)/1e12,'og', label  = 'addition of both')
+    plt.axvline(rd_dist)
+    plt.axvline(2.0*rd_dist)
     #plt.plot(rdbin,h_p.sigma(rdbin))
     #plt.xlim(0.05,)
     #plt.ylim(1e0,)
-    #plt.xscale('log')
+    plt.xscale('log')
     #plt.yscale('log')
     #plt.xlabel('R (Mpc h-1)')
     #plt.ylabel(r'$\Delta \Sigma (R) \times 10^{12}$ ')
